7897a63b13caf8fef6304308c0a216882d471688
Greg Church <greggypc@users.noreply.github.com>
2017-12-04 19:01:07 -0600

    added styling / fixed timer issue

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,43 +3,54 @@
     var triviaQs = [{
 	 	song: "assets/audio/andy_griffith-t.mp3",
     choices: ["Happy Days", "I Love Lucy", "Gomer Pyle", "The Andy Griffith Show"],
-	 	correctAnswer: "D"
+	 	correctAnswer: "D",
+    image: "assets/images/andy-griffith-img.jpg"
 	},{
     song: "assets/audio/mash-t.mp3",
 	 	choices: ["Hill Street Blues", "M.A.S.H.", "Taxi", "A-Team"],
-	 	correctAnswer: "B"
+	 	correctAnswer: "B",
+    image: "assets/images/mash-img.jpg"
 	},{
     song: "assets/audio/mad_men-t.mp3",
 	 	choices: ["Mad Men", "Orange is the New Black", "Sons of Anarchy", "Dr. Who"],
-	 	correctAnswer: "A"
+	 	correctAnswer: "A",
+    image: "assets/images/mad-men-img.jpg"
 	},{
     song: "assets/audio/threes_company-t.mp3",
 	 	choices: ["Diff'rent Strokes", "Bosom Buddies", "Three's Company", "Love Boat"],
-	 	correctAnswer: "C"	 	
+	 	correctAnswer: "C",
+    image: "assets/images/threes-company-img.jpg"	 	
 	},{
     song: "assets/audio/king_of_the_hill-t.mp3",
     choices: ["Sanford & Son", "Reba", "The Simpsons", "King of the Hill"],
-    correctAnswer: "D"    
+    correctAnswer: "D",
+    image: "assets/images/king-of-the-hill-img.jpg"    
   },{
     song: "assets/audio/knightrider-t.mp3",
     choices: ["Hawaii 5-0", "Knight Rider", "Battlestar Galactica", "Airwolf"],
-    correctAnswer: "B"    
+    correctAnswer: "B",
+    image: "assets/images/knight-rider-img.jpg"    
   },{
     song: "assets/audio/breaking_bad-t.mp3",
     choices: ["Better Call Saul", "The Americans", "Seinfeld", "Breaking Bad"],
-    correctAnswer: "D"    
+    correctAnswer: "D",
+    image: "assets/images/breaking-bad-img.jpg"    
   },{
     song: "assets/audio/magnum_tune-t.mp3",
     choices: ["Magnum P.I.", "Alf", "Simon & Simon", "The Facts of Life"],
-    correctAnswer: "A"    
+    correctAnswer: "A",
+    image: "assets/images/magnum-pi-img.jpg"    
   },{
     song: "assets/audio/beverley_hillbillies-t.mp3",
     choices: ["Lavern & Shirley", "Green Acres", "The Beverly Hillbillies", "Jeb's Po' Fam'ly"],
-    correctAnswer: "C"    
+    correctAnswer: "C",
+    image: "assets/images/beverly-hillbillies-img.jpg"    
   },{
     song: "assets/audio/the_walking_dead-t.mp3",
     choices: ["Fear the Walking Dead", "Dexter", "Game of Thrones", "The Walking Dead"],
-    correctAnswer: "D"    
+    correctAnswer: "D",
+    image: "assets/images/walking-dead-img.jpg"     
+
   }];
 
 var i = 0;
@@ -64,10 +75,11 @@ $(document).ready(function() {
 
   function startTimer15() {
         count = 11;
-        counter = setInterval(timer, 1000); //1000 will  run it every 1 second
+        counter = setInterval(timer, 1000); 
         function timer() {
             
             count--;
+            $("#timer").show();
             $("#timer").html("Time Remaining: " + count); 
             if (count < 1) {
                 clearInterval(counter);
@@ -90,8 +102,7 @@ $(document).ready(function() {
       displayGameResults();
     }
   }
-            //trim my songs to 9 secs and make timer 10 sec. Still include .stop();
-
+    
   function getQuestion() {
     console.log("getQuestion function running");
     $("#display").empty();
@@ -129,13 +140,16 @@ $(document).ready(function() {
   function timeRanOut() {
     $("#timer").hide();
     if (i < 9) {
-      $("#display").html('<div>Too slow!</div>' +
-      '<div>Gotta think quick! Here comes the next clip...</div>');
+      $("#display").html('<div class="message">Too slow!</div>');
+      $("#display").append('<div class="message">Gotta think quick! Here comes the next clip...</div>');
+      $("#display").append('<img id="answerImg" src=' + triviaQs[i].image + ' />');
+     
     unanswered++;
     setTimeout(timerNextQ, 5000);
   }else {
     $("#display").html('<div>Too slow!</div>' +
-      '<div>Game Over!</div>' +  '<div>Here come your results...</div>');
+      '<div class="message">Game Over!</div>' +  '<div class="message">Here come your results...</div>');
+     $("#display").append('<img id="answerImg" src=' + triviaQs[i].image + ' />');
     unanswered++;
     setTimeout(timerNextQ, 5000);
   }
@@ -145,12 +159,14 @@ $(document).ready(function() {
   function userCorrect() {
     $("#timer").hide();
     if (i < 9) {
-    $("#display").html('<div>Correct!</div>' +
-      '<div>Here comes the next clip...</div>');
+    $("#display").html('<div class="message">Correct!</div>' +
+      '<div class="message">Here comes the next clip...</div>');
+     $("#display").append('<img id="answerImg" src=' + triviaQs[i].image + ' />');
     setTimeout(timerNextQ, 5000);
     }else {
-    $("#display").html('<div>Correct!</div>' +
-      '<div>Game Over!</div>' +  '<div>Here come your results...</div>');
+    $("#display").html('<div class="message">Correct!</div>' +
+      '<div class="message">Game Over!</div>' +  '<div class="message">Here come your results...</div>');
+     $("#display").append('<img id="answerImg" src=' + triviaQs[i].image + ' />');
     setTimeout(timerNextQ, 5000);
   }
   };
@@ -158,12 +174,14 @@ $(document).ready(function() {
   function userWrong() {
     $("#timer").hide();
     if (i < 9) {
-    $("#display").html('<div>Wrong!</div>' +
-      '<div>Keep trying! Here comes the next clip...</div>');
+    $("#display").html('<div class="message">Wrong!</div>' +
+      '<div class="message">Keep trying! Here comes the next clip...</div>');
+     $("#display").append('<img id="answerImg" src=' + triviaQs[i].image + ' />');
     setTimeout(timerNextQ, 5000);
     }else {
-    $("#display").html('<div>Wrong!</div>' +
-      '<div>Game Over!</div>' +  '<div>Here come your results...</div>');
+    $("#display").html('<div class="message">Wrong!</div>' +
+      '<div class="message">Game Over!</div>' +  '<div class="message">Here come your results...</div>');
+     $("#display").append('<img id="answerImg" src=' + triviaQs[i].image + ' />');
     setTimeout(timerNextQ, 5000);
   }
   };
@@ -171,18 +189,18 @@ $(document).ready(function() {
   function displayGameResults () {
     $("#timer").hide();
     if (right > wrong && unanswered) {
-       $("#display").html('<div>Nice work!</div>');
+       $("#display").html('<div class="message">Nice work!</div>');
       $("#display").append(
-       '<div>Correct Answers: ' + right + '</div>' +
-       '<div>Incorrect Answers: ' + wrong + '</div>' +
-       '<div>Unanswered Questions: ' + unanswered + '</div>');
+       '<div class="message">Correct Answers: ' + right + '</div>' +
+       '<div class="message">Incorrect Answers: ' + wrong + '</div>' +
+       '<div class="message">Unanswered Questions: ' + unanswered + '</div>');
        playAgain();
     }else if (right <= wrong) {
-       $("#display").html('<div>No so great...</div>');
+       $("#display").html('<div class="message">No so great...</div>');
        $("#display").append(
-       '<div>Correct Answers: ' + right + '</div>' +
-       '<div>Incorrect Answers: ' + wrong + '</div>' +
-       '<div>Unanswered Questions: ' + unanswered + '</div>');
+       '<div class="message">Correct Answers: ' + right + '</div>' +
+       '<div class="message">Incorrect Answers: ' + wrong + '</div>' +
+       '<div class="message">Unanswered Questions: ' + unanswered + '</div>');
        playAgain();
       }
 
@@ -190,7 +208,7 @@ $(document).ready(function() {
     };
 
     function playAgain() {
-      $("#display").append('<button>Play Again!</button>');
+      $("#display").append('<button id="playAgain">Play Again!</button>');
       $("button").on("click", function() {
       right = 0;
       wrong = 0;
